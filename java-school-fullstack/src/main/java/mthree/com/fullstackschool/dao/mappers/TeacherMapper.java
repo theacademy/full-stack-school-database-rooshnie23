497baf271785778b76d487f76636ab15d82f40c3
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-19 12:16:21 -0400

    commit 1: changed all the mappers

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/mappers/TeacherMapper.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/mappers/TeacherMapper.java
@@ -9,9 +9,12 @@ public class TeacherMapper implements RowMapper<Teacher> {
     @Override
     public Teacher mapRow(ResultSet rs, int rowNum) throws SQLException {
         //YOUR CODE STARTS HERE
-
-
-        return null;
+        Teacher teacher = new Teacher();
+        teacher.setTeacherId(rs.getInt("tid"));
+        teacher.setTeacherFName(rs.getString("tFName"));
+        teacher.setTeacherLName(rs.getString("tLName"));
+        teacher.setDept(rs.getString("dept"));
+        return teacher;
 
         //YOUR CODE ENDS HERE
     }
