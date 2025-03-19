63879e64217cd0b08233880a446060ae640b72ab
rooshnie23 <rooshnie.velautham@mail.mcgill.ca>
2025-03-19 17:02:13 -0400

    commit 6: finished CourseServiceImpl

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/CourseServiceImpl.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/CourseServiceImpl.java
@@ -12,6 +12,14 @@ public class CourseServiceImpl implements CourseServiceInterface {
 
     //YOUR CODE STARTS HERE
 
+    private final CourseDao courseDao;
+
+
+    @Autowired
+    public CourseServiceImpl(CourseDao courseDao) {
+        this.courseDao = courseDao;
+    }
+
 
 
     //YOUR CODE ENDS HERE
@@ -19,7 +27,7 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public List<Course> getAllCourses() {
         //YOUR CODE STARTS HERE
 
-        return null;
+        return courseDao.getAllCourses();
 
         //YOUR CODE ENDS HERE
     }
@@ -27,7 +35,14 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public Course getCourseById(int id) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        try {
+            return courseDao.findCourseById(id);
+        } catch (DataAccessException e) {
+            Course course = new Course();
+            course.setCourseName("Course Not Found");
+            course.setCourseDesc("Course Not Found");
+            return course;
+        }
 
         //YOUR CODE ENDS HERE
     }
@@ -35,7 +50,14 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public Course addNewCourse(Course course) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        if (course.getCourseName() == null || course.getCourseName().isEmpty() ||
+            course.getCourseDesc() == null || course.getCourseDesc().isEmpty()) {
+
+            course.setCourseName("Name blank, course NOT added");
+            course.setCourseDesc("Description blank, course NOT added");
+            return course;
+        }
+        return courseDao.createNewCourse(course);
 
         //YOUR CODE ENDS HERE
     }
@@ -43,7 +65,13 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public Course updateCourseData(int id, Course course) {
         //YOUR CODE STARTS HERE
 
-        return null;
+        if (id != course.getCourseId()) {
+            course.setCourseName("IDs do not match, course not updated");
+            course.setCourseDesc("IDs do not match, course not updated");
+            return course;
+        }
+        courseDao.updateCourse(course);
+        return course;
 
         //YOUR CODE ENDS HERE
     }
@@ -51,7 +79,8 @@ public class CourseServiceImpl implements CourseServiceInterface {
     public void deleteCourseById(int id) {
         //YOUR CODE STARTS HERE
 
-
+        courseDao.deleteCourse(id);
+        System.out.println("Course ID: " + id + " deleted");
 
         //YOUR CODE ENDS HERE
     }
